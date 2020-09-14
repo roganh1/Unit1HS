e9b423ccc3c43b1a4af212be283fd0f8706e5f85
roganhelm <817830@mystma.org>
2020-09-14 20:15:26 +0000

    more stuff

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -2,6 +2,18 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        System.out.println("Hello World");
+        System.out.println("This is my first line of java");
+        System.out.println("The matrix");
+        System.out.println("Dodgeball");
+        System.out.println("E.T.");
+        System.out.println("It");
+        System.out.println("Mrs. Doubtfire");
+        System.out.println("Starwars Ep. 1");
+        System.out.println("Snowden");
+        System.out.println("The Hurt Locker");
+        System.out.println("Black Hawk Down");
+        System.out.println("Jaws");
+        System.out.println("");
     }
 }
