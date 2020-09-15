c5a44781af7388aed22046d84483ec22e646f13b
roganhelm <817830@mystma.org>
2020-09-15 20:09:13 +0000

    primitive data types

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -2,18 +2,30 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World");
-        System.out.println("This is my first line of java");
-        System.out.println("The matrix");
-        System.out.println("Dodgeball");
-        System.out.println("E.T.");
-        System.out.println("It");
-        System.out.println("Mrs. Doubtfire");
-        System.out.println("Starwars Ep. 1");
-        System.out.println("Snowden");
-        System.out.println("The Hurt Locker");
-        System.out.println("Black Hawk Down");
-        System.out.println("Jaws");
-        System.out.println("");
+        boolean livesInStMichael = true;
+        char firstLetterOfName = 'R';
+        byte age = 17;
+        short avgNumberOfSteps = 20000;
+        int myFavNum = 60000;
+        long bigNum = 7000000;
+        float pi = 3.14f;
+        double rand = 4.54167535415875;
+
+        System.out.println("Whats a random number?");
+        System.out.println(rand);
+        System.out.println("What are the first three digits of pi?");
+        System.out.println(pi);
+        System.out.println("Give me a big number");
+        System.out.println(bigNum);
+        System.out.println("What's my favorite number?");
+        System.out.println(myFavNum);
+        System.out.println("About how many steps do I take weekly?");
+        System.out.println(avgNumberOfSteps);
+        System.out.println("How old am I?");
+        System.out.println(age);
+        System.out.println("What's the first letter of my name?");
+        System.out.println(firstLetterOfName);
+        System.out.println("Do I live in St. Michael?");
+        System.out.println(livesInStMichael);
     }
 }
