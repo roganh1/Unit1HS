545188dbd87ec5801f1c5a354c13630704725468
roganhelm <817830@mystma.org>
2020-09-21 19:54:47 +0000

    Madlibs with user input

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,22 +1,17 @@
 package app;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String person1 = "Rogan";
-        String person2 = "Mr. Osowski";
-        String verb1 = "eat";
-        String verb2 = "flatten";
-        String verb3 = "orbit";
-        String food = "noodles";
-        String dayOfWeek = "Monday";
-        String place1 = "library";
-        String place2 = "landfill";
-        String adjective1 = "huge";
-        String adjective2 = "dry";
 
-        System.out.println(person1 + " " + "is a programmer who likes to" + " " + verb1 + ".");
-        System.out.println("Every" + " " + dayOfWeek + " " + "they make a" + " " + adjective1 + " " + food + " " + "for breakfast and" + " " + verb2 + " " + "at" + " " + place1 + " " + "with" + " " + person2 + ".");
-        System.out.println("Sometimes, they go to the" + " " + adjective2 + " " + place2 + " " + "with" + " " + person2 + " " + "so that they can" + " " + verb3 + " " + "together.");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a noun.");
+        String noun1 = input.nextLine();
+        System.out.println("Enter an adjective.");
+        String adj1 = input.nextLine();
+
+        System.out.println("The" + " " + noun1 + " " + "is" + " " + adj1 + ".");
 
     }
 }
