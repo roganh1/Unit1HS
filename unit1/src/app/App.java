173cb6349eed8e30c5357c308b46b75f0f0984c4
roganhelm <817830@mystma.org>
2020-09-23 18:16:49 +0000

    while loops

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -5,10 +5,18 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Please enter a color");
+        System.out.println("Please enter a color.");
         String color = input.nextLine();
 
-        System.out.println("Please enter a number");
+        System.out.println("Please enter a number.");
+        int num = input.nextInt();
+
+        int total = 0;
+
+        while (total < num) {
+            System.out.println(color);
+            total = total + 1;
         }
+
     }
 }
