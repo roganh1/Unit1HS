70c4fdc531c5adaa9fd6d953055aed763599b0d9
roganhelm <817830@mystma.org>
2020-09-30 17:14:08 +0000

    Five question quiz

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,39 +1,63 @@
 package app;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Let's count from 10 to 20");
-        
-        for (int i = 10; i <= 20; i = i +1){
-            System.out.println("The count is " +i);
-            System.out.println("Let's keep counting.");
-        }
+        Scanner input = new Scanner(System.in);
+        byte score = 0;
 
-        for (int i = 10; i <= 100; i = i +1){
-            System.out.println("The count is " +i);
-            System.out.println("Let's keep counting.");
-        }
+        System.out.println("Do you want to take this quiz?");
+        String answer1 = input.nextLine();
 
-        System.out.println("Now let's do 1,000-256,000");
+            if (answer1.equals("Yes")) {
+                System.out.println("Nice, good for you");
+                score++;
+            } else {
+                System.out.println("Why are you here?");
+            }
 
-        for (int i = 1000; i <= 256000; i = i +1){
-            System.out.println("The count is " +i);
-            System.out.println("Let's keep counting.");
-        }
+        System.out.println("What's 10 / 5");
+        String answer2 = input.nextLine();
+            
+            if (answer2.equals("2")) {
+                System.out.println("Right!");
+                score++;
+            } else {
+                System.out.println("Oops wrong answer");
+            }
 
-        System.out.println("Now 11-12");
+        System.out.println("What's my name?");
+        String answer3 = input.nextLine();
 
-        for (int i = 11; i <= 12; i = i +1){
-            System.out.println("The count is " +i);
-            System.out.println("Let's keep counting.");
-        }
+            if (answer3.equals("Rogan")) {
+                System.out.println("Right!");
+                score++;
+            } else {
+                System.out.println("Oops wrong answer");
+            }
 
-        System.out.println("Now lets try something a bit different.");
+        System.out.println("What language was this quiz written with?");
+        String answer4 = input.nextLine();
 
-        for (int i = 10; i <= 10; i = i -1){
-            System.out.println("The count is " +i);
-            System.out.println("Let's keep counting.");
-        }
+            if (answer4.equals("Java")) {
+                System.out.println("Right!");
+                score++;
+            } else {
+                System.out.println("Oops wrong answer");
+            }
 
+        System.out.println("Was this quiz easy?");
+        String answer5 = input.nextLine();
+
+            if (answer5.equals("Yes")) {
+                System.out.println("Right!");
+                score++;
+            } else {
+                System.out.println("Oops wrong answer");
+            }
+
+        System.out.println(score + "/5");
+        System.out.println((score/5.0)*100);
     }
 }
