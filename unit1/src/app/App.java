d5775ae7091461a2882b9b5b94c71871bbed30cb
roganhelm <817830@mystma.org>
2020-10-01 18:14:05 +0000

    Animal Facts!

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -4,60 +4,37 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to bird facts! Please choose a fact (1-5)");
+
         Scanner input = new Scanner(System.in);
-        byte score = 0;
+        byte b = input.nextByte();
 
-        System.out.println("Do you want to take this quiz?");
-        String answer1 = input.nextLine();
+        if (b < 1) {
+            System.out.println("Error");
+        }
 
-            if (answer1.equals("Yes")) {
-                System.out.println("Nice, good for you");
-                score++;
-            } else {
-                System.out.println("Why are you here?");
-            }
+        if (b > 5) {
+            System.out.println("Error");
+        }
 
-        System.out.println("What's 10 / 5");
-        String answer2 = input.nextLine();
-            
-            if (answer2.equals("2")) {
-                System.out.println("Right!");
-                score++;
-            } else {
-                System.out.println("Oops wrong answer");
-            }
+        if (b == 1) {
+            System.out.println("Fact #1, birds can fly!");
+        }
 
-        System.out.println("What's my name?");
-        String answer3 = input.nextLine();
+        if (b == 2) {
+            System.out.println("Fact #2, birds have semi-hollow bones");
+        }
 
-            if (answer3.equals("Rogan")) {
-                System.out.println("Right!");
-                score++;
-            } else {
-                System.out.println("Oops wrong answer");
-            }
+        if (b == 3) {
+            System.out.println("Fact #3, about 2/3 of all bird species are found in tropical rain forests");
+        }
 
-        System.out.println("What language was this quiz written with?");
-        String answer4 = input.nextLine();
+        if (b == 4) {
+            System.out.println("Fact #4, there is over 9,500 species of birds");
+        }
 
-            if (answer4.equals("Java")) {
-                System.out.println("Right!");
-                score++;
-            } else {
-                System.out.println("Oops wrong answer");
-            }
-
-        System.out.println("Was this quiz easy?");
-        String answer5 = input.nextLine();
-
-            if (answer5.equals("Yes")) {
-                System.out.println("Right!");
-                score++;
-            } else {
-                System.out.println("Oops wrong answer");
-            }
-
-        System.out.println(score + "/5");
-        System.out.println((score/5.0)*100+"%");
+        if (b == 5) {
+            System.out.println("Fact #5, a pelican's beak can hold up to 2.5 gallons at a time");
+        }
     }
 }
