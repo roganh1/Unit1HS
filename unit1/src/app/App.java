671b91d37ef4d23c2554e80664289964716858a2
roganhelm <817830@mystma.org>
2020-10-06 19:53:28 +0000

    Challenge 2

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,12 +1,33 @@
 package app;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        int count = 0;
+        boolean done = false;
+        byte numIncorrect = 0;
 
-        while (count <= 1000) {
-            System.out.println(count);
-            count = count +1;
+        while (done == false) {
+            System.out.println("What's the password?");
+
+        Scanner input = new Scanner(System.in);
+        String passwd = input.nextLine();
+
+            if (numIncorrect < 3) {
+                if (passwd.equals("Password")) {
+                    System.out.println("Here's the secret message!");
+                    done = true;
+                } else {
+                    System.out.println("Error");
+                    System.out.println("Please try again");
+                    numIncorrect++;
+                }
+            }
+
+            if (numIncorrect >= 3) {
+                System.out.println("You have exceeded the maximum number of tries");
+                done = true;
+            }
         }
     }
 }
